8edf0ccf074ffdd1a0e6a25bfadad040c910e79f
Ходырев Роман <khodyyrevrv@student.bmstu.ru>
2023-09-10 01:26:30 +0300

    Решение биквадратного уравнения + обр. исключений

--- lab1/main.py
+++ lab1/main.py
@@ -1,6 +1,65 @@
-print("New repository")
-print("Вы хотите завершить работу?")
-ans = input()
-while ans!="Да":
-    print("Подумайте еще раз")
-    ans = input()
+import sys
+import math
+import time
+
+KOEFS = {
+    1:'a',
+    2:'b',
+    3:'c'
+}
+
+#Получаем коэффициенты из командной строки или считываем из консоли
+def get_koef(ind, prompt):
+    try:
+        coef = int(sys.argv[ind])
+    except:
+        print(f"Введите коэффициент {prompt.upper()}: ", end="")
+        coef = float(input())
+    return coef
+
+#Получаем корни биквадратного уравнения
+def get_roots(a,b,c):
+    roots = []
+    D = float(b**2-4*a*c)
+    if D > 0.0:
+        rt_1 = (-b+math.sqrt(D))/(2*a)
+        rt_2 = (-b-math.sqrt(D))/(2*a)
+        roots.extend([rt_1, rt_2])
+    elif D==0.0:
+        rt_1 = (-b)/(2*a)
+        roots.append(rt_1)
+    return roots
+
+#Если А=0, то решаем линейное уравнение
+def unbiquadarate(b,c):
+    roots = [(-c)/b]
+    return roots
+
+def main():
+    a,b,c = [get_koef(i, KOEFS[i]) for i in range(1,4)]
+    if a!=0:
+        roots = get_roots(a,b,c)
+    elif b!=0:
+        roots = unbiquadarate(b,c)
+    elif c!=0:
+        roots = []
+    else:
+        print("x - любое")
+        time.sleep(10)
+        return
+
+    #Обработка результата
+    if len(roots)!=0:
+        if len(roots) == 2:
+            print("Два действительных корня")
+            print(f"Первый корень: {roots[0]}")
+            print(f"Второй корень: {roots[1]}")
+        else:
+            print(f"Один действительный корень: {roots[0]}")
+    else:
+        print("Нет корней")
+    
+    time.sleep(10)
+
+if __name__ == "__main__":
+    main()
