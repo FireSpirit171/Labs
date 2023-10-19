31ba40a32a8e4961a7c411e50fa49f69fc3059f3
Ходырев Роман <khodyyrevrv@student.bmstu.ru>
2023-10-20 02:57:44 +0300

    Второе задание

--- lab4/gen_random.py
+++ lab4/gen_random.py
@@ -7,7 +7,7 @@ def gen_random(num, min, max):
         times = 0
         while times!=num:
             yield randint(min, max)
-            times+=1
+            times += 1
 
 if __name__ == "__main__":
     result = [x for x in gen_random(5, 1, 3)]
