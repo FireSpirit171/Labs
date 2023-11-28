3994dbcc32458c96af8d92c476e25d5c3f47aa80
Ходырев Роман <khodyyrevrv@student.bmstu.ru>
2023-11-29 01:15:59 +0300

    Создан HTML шаблон

--- project/go_website/main.go
+++ project/go_website/main.go
@@ -2,11 +2,23 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
 
+type Film struct {
+	name  string
+	plot  string
+	photo string
+}
+
 func home_page(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello website")
+	film1 := Film{"Interstellar", "When Earth becomes uninhabitable in the future," +
+		"a farmer and ex-NASA pilot, Joseph Cooper, is tasked to pilot a spacecraft," +
+		"along with a team of researchers, to find a new planet for humans.",
+		"https://m.media-amazon.com/images/I/A1JVqNMI7UL._AC_UF1000,1000_QL80_.jpg"}
+	tmp, _ := template.ParseFiles("templates/home_page.html")
+	tmp.Execute(w, film1)
 }
 
 func autor_page(w http.ResponseWriter, r *http.Request) {
