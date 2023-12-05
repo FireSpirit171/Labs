181ff9898f54a84d37a6c16c4ebc69cee7bc73b6
Ходырев Роман <khodyyrevrv@student.bmstu.ru>
2023-12-05 23:23:10 +0300

    Подключил БД MySQL

--- project/go_website/main.go
+++ project/go_website/main.go
@@ -1,17 +1,21 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"html/template"
 	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type Film struct {
-	Name  string
-	Plot  string
-	Photo string
+	Name  string `json:"name"`
+	Plot  string `json:"plot"`
+	Photo string `json:"photo"`
 }
 
+/*
 var film1 = Film{
 	"Interstellar",
 	"When Earth becomes uninhabitable in the future, a farmer and ex-NASA pilot, Joseph Cooper, is tasked to pilot a spacecraft, along with a team of researchers, to find a new planet for humans.",
@@ -31,7 +35,7 @@ var film3 = Film{
 
 var film4 = Film{
 	"Arrival",
-	"When mysterious spacecraft touch down across the globe, an elite team, led by linguist Louise Banks, is brought together to decipher their intent. As tensions rise between nations, Banks races against time to understand the aliens' language to prevent a global war.",
+	"When mysterious spacecraft touch down across the globe, an elite team, led by linguist Louise Banks, is brought together to decipher their intent. As tensions rise between nations, Banks races against time to understand the aliens language to prevent a global war.",
 	"images/film/Arrival.jpg",
 }
 
@@ -41,12 +45,12 @@ var film5 = Film{
 	"images/film/TheMatrix.jpg",
 }
 
-var Films = []Film{film1, film2, film3, film4, film5}
+var Films = []Film{film1, film2, film3, film4, film5}*/
 
 func home_page(w http.ResponseWriter, r *http.Request) {
 	http.Handle("/images/film/", http.StripPrefix("/images/film/", http.FileServer(http.Dir("./images/film"))))
 	tmp, _ := template.ParseFiles("templates/home_page.html")
-	tmp.Execute(w, Films)
+	tmp.Execute(w, w) //Здесь нужно заменит второй w
 }
 
 func autor_page(w http.ResponseWriter, r *http.Request) {
@@ -56,9 +60,39 @@ func autor_page(w http.ResponseWriter, r *http.Request) {
 func handleRequest() {
 	http.HandleFunc("/", home_page)
 	http.HandleFunc("/autor/", autor_page)
-	http.ListenAndServe(":8000", nil)
+	http.ListenAndServe("localhost:8000", nil)
 }
 
 func main() {
-	handleRequest()
+	//handleRequest()
+
+	db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/film_reviews")
+	if err != nil {
+		panic(err)
+	}
+
+	defer db.Close()
+
+	/* Новый фильм
+	insert, err := db.Query("INSERT INTO films (name, plot, photo) VALUES ('Arrival', 'When mysterious spacecraft touch down across the globe, an elite team, led by linguist Louise Banks, is brought together to decipher their intent. As tensions rise between nations, Banks races against time to understand the aliens language to prevent a global war.', 'images/film/Arrival.jpg')")
+	if err != nil {
+		panic(err)
+	}
+	insert.Close()*/
+
+	//Получение фильмов
+	res, err := db.Query("SELECT name, plot, photo FROM films")
+	if err != nil {
+		panic(err)
+	}
+
+	for res.Next() {
+		var film Film
+		err = res.Scan(&film.Name, &film.Plot, &film.Photo)
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Println(fmt.Sprintf("Name: %s\nPlot: %s\nPhoto: %s", film.Name, film.Plot, film.Photo))
+	}
 }
