770052c57cf9b4dc70e91731eee2185e81f4b979
Ходырев Роман <khodyyrevrv@student.bmstu.ru>
2023-12-10 05:22:00 +0300

    Динамические страницы с обзорами фильмов

--- project/go_website/main.go
+++ project/go_website/main.go
@@ -10,16 +10,17 @@ import (
 	"strings"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
 )
 
 type Film struct {
+	Id    uint16 `json:"id"`
 	Name  string `json:"name"`
 	Plot  string `json:"plot"`
 	Photo string `json:"photo"`
 }
 
 func home_page(w http.ResponseWriter, r *http.Request) {
-	mux := http.NewServeMux()
 	tmp, err := template.ParseFiles("templates/home_page.html")
 	if err != nil {
 		panic(err)
@@ -31,7 +32,7 @@ func home_page(w http.ResponseWriter, r *http.Request) {
 	}
 	defer db.Close()
 
-	dbfilms, err := db.Query("SELECT name, plot, photo FROM films")
+	dbfilms, err := db.Query("SELECT id, name, plot, photo FROM films")
 	if err != nil {
 		panic(err)
 	}
@@ -39,7 +40,7 @@ func home_page(w http.ResponseWriter, r *http.Request) {
 	Films := []Film{}
 	for dbfilms.Next() {
 		var film Film
-		err = dbfilms.Scan(&film.Name, &film.Plot, &film.Photo)
+		err = dbfilms.Scan(&film.Id, &film.Name, &film.Plot, &film.Photo)
 		if err != nil {
 			panic(err)
 		}
@@ -47,7 +48,11 @@ func home_page(w http.ResponseWriter, r *http.Request) {
 	}
 
 	tmp.ExecuteTemplate(w, "home_page", Films)
-	http.ListenAndServe("localhost:8000", mux)
+	http.ListenAndServe("localhost:8000", nil)
+}
+
+func autor_page(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("The autor of this website")
 }
 
 func new_review_page(w http.ResponseWriter, r *http.Request) {
@@ -59,18 +64,13 @@ func new_review_page(w http.ResponseWriter, r *http.Request) {
 	tmp.ExecuteTemplate(w, "new_review", nil)
 }
 
-func autor_page(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("The autor of this website")
-}
-
 func save_new_review(w http.ResponseWriter, r *http.Request) {
-
 	r.ParseMultipartForm(10 << 20)
 	name := r.FormValue("filmName")
 	plot := r.FormValue("plot")
 	file, _, err := r.FormFile("posterLink")
 
-	cname := strings.ReplaceAll(name, " ", "") //Убираем из названия пробелы
+	cname := strings.ReplaceAll(name, " ", "_") //Убираем из названия пробелы
 	//Далее обработка полученного в поле постер файла
 	if err != nil {
 		panic(err)
@@ -102,13 +102,51 @@ func save_new_review(w http.ResponseWriter, r *http.Request) {
 	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
 
+func show_review(w http.ResponseWriter, r *http.Request) {
+	tmp, err := template.ParseFiles("templates/review.html")
+	if err != nil {
+		panic(err)
+	}
+
+	vars := mux.Vars(r) // id фильма который мы хотим увидеть
+
+	db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:3306)/film_reviews")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	res, err := db.Query(fmt.Sprintf("SELECT * FROM films WHERE id = '%s'", vars["id"]))
+	if err != nil {
+		panic(err)
+	}
+
+	var showFilm Film //Фильм который будем показывать
+	for res.Next() {
+		var film Film
+		err := res.Scan(&film.Id, &film.Name, &film.Plot, &film.Photo)
+		if err != nil {
+			panic(err)
+		}
+
+		showFilm = film //Нашли нужный фильм
+	}
+
+	tmp.ExecuteTemplate(w, "review", showFilm)
+}
+
 func handleFunc() {
 	http.Handle("/images/film/", http.StripPrefix("/images/film/", http.FileServer(http.Dir("./images/film"))))
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
-	http.HandleFunc("/", home_page)
-	http.HandleFunc("/new_review/", new_review_page)
-	http.HandleFunc("/save_new_review/", save_new_review)
-	http.HandleFunc("/autor_page/", autor_page)
+
+	rtr := mux.NewRouter()
+	rtr.HandleFunc("/", home_page).Methods("GET")
+	rtr.HandleFunc("/new_review/", new_review_page).Methods("GET")
+	rtr.HandleFunc("/save_new_review/", save_new_review).Methods("POST")
+	rtr.HandleFunc("/autor_page/", autor_page).Methods("GET")
+	rtr.HandleFunc("/review/{id:[0-9]+}", show_review).Methods("GET")
+
+	http.Handle("/", rtr)
 	http.ListenAndServe("localhost:8000", nil)
 }
 
